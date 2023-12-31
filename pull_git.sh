f9566c9db00a2b6f35b778c8d100a45d7034a3b2
spamhws <egorsanko@gmail.com>
2023-12-31 00:10:30 +0000

    gh_key update

--- pull_git.sh
+++ pull_git.sh
@@ -1,3 +1,19 @@
-#! /usr/bin/bash
+#!/bin/bash
 
-git pull origin main
+# Check if SSH agent is running
+if [ -z "$SSH_AUTH_SOCK" ]; then
+  # Start SSH agent and set SSH_AUTH_SOCK variable
+  eval $(ssh-agent)
+fi
+
+# Check if SSH key is added
+if ! ssh-add -l | grep -q '~/.ssh/gh_key'; then
+  # Add SSH key to agent
+  ssh-add ~/.ssh/gh_key
+fi
+
+# Change to the desired directory
+cd /var/www/app/
+
+# Run Git pull command
+git pull origin main --ff-only
